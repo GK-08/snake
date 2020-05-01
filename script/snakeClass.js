9761daee4d24f0d83279dfec86ed9fb5823f0c44
Giorgi Kiltava <gkiltava@gmail.com>
2020-05-01 12:14:13 +0400

    snake class file code cleaned and commented

--- script/snakeClass.js
+++ script/snakeClass.js
@@ -5,10 +5,17 @@ const DOWN=40;
 const BOARD_WIDTH=board.offsetWidth;
 const BOARD_HEIGHT=board.offsetHeight;
 const CURRENT_SCORE=document.getElementById(`current-score`);
-const BEST_SCORE=document.getElementById(`best-score`);//is it really needed?
+const BEST_SCORE=document.getElementById(`best-score`);
+const FOOD=document.getElementById(`food`);
 
 
 class Snake{
+    /*
+    *                       constructor()
+    * Creates snakeHead, appends to board, calls initialize() function [line 39],
+    * creates an array where all the cells will be saved with snakeHead,
+    * creates starting four cells (snakeHead excluded)
+    */
     constructor(){
         this.snakeHead=document.createElement(`div`);//shall I write these with backtics?
         this.snakeHead.classList.add(`snake-cell`);
@@ -23,21 +30,29 @@ class Snake{
             board.appendChild(cell);
             this.snake.push(cell);
         }
-        this.food=document.getElementById(`food`);
-        this.moving;
     }
+    /*
+    *                   initialize()
+    * Defines snakeHead starting position and direciton;
+    * Sets score to 0 and isMoving to false;
+    */
     initialize(){
         this.snakeHead.style.top=`0px`;
         this.snakeHead.style.left=`40px`;
         this.direction=RIGHT;
         this.score=0;
         this.isMoving=false;
-        CURRENT_SCORE.innerHTML=`Score: ${this.score}`;
     }
+    /*
+    *                       reset()
+    * If the best score is beaten, updates and displays the new record;
+    * Calls initialize() function [line:39];
+    * Removes gained cells from array and DOM;
+    * Resets starting position for starting cells (including snakeHead);
+    */
     reset(){
-        clearInterval(this.moving);
-        if(this.score>localStorage.getItem('highScore')){
-            localStorage.setItem('highScore',this.score);
+        if(this.score>localStorage.getItem('bestScore')){
+            localStorage.setItem('bestScore',this.score);
             BEST_SCORE.innerHTML=`Best: ${this.score}`;
         }
         this.initialize();
@@ -53,6 +68,82 @@ class Snake{
         }
 
     }
+    /*
+    *                               move()
+    * Changes cells' positions with regard to previous cell position;
+    * Changes snakeHead position with regard to direction;
+    * Creates and keeps last direction that helps to prevent accepting opposite direction;
+    */
+    move(){
+        for(let i=this.snake.length-1; i>0; i--){
+            this.snake[i].style.left=`${this.snake[i-1].offsetLeft}px`;
+            this.snake[i].style.top=`${this.snake[i-1].offsetTop}px`;
+        }
+        switch(this.direction){
+            case LEFT:
+                this.snakeHead.style.left=`${this.snakeHead.offsetLeft-10}px`;
+                break;
+            case UP:
+                this.snakeHead.style.top=`${this.snakeHead.offsetTop-10}px`;
+                break;
+            case RIGHT:
+                this.snakeHead.style.left=`${this.snakeHead.offsetLeft+10}px`;
+                break;
+            case DOWN:
+                this.snakeHead.style.top=`${this.snakeHead.offsetTop+10}px`;
+                break;
+        }
+        this.lastDirection=this.direction;
+    }
+    /*
+    *               checkIfAteFood()
+    * Checks if food is eaten and if so, @returns true;
+    * @returns false by default;
+    */
+    checkIfAteFood(){
+        if(this.snakeHead.offsetLeft===FOOD.offsetLeft && 
+            this.snakeHead.offsetTop===FOOD.offsetTop){
+                return true;
+            }
+        return false;
+    }
+    /*
+    *               createFood()
+    * Gets random numbers to define food position;
+    * "Fixes" numbers to be 10 multiple;
+    * Defines food positionl
+    */
+    createFood(){
+        let X=Math.floor(Math.random()*BOARD_WIDTH);
+        let Y=Math.floor(Math.random()*BOARD_HEIGHT);
+        X-=(X%10);
+        Y-=(Y%10);
+        food.style.left=`${X}px`;
+        food.style.top=`${Y}px`;
+    }
+    /*
+    *                   updateScore()
+    * Adds 10 points to score and displays the updated one.
+    */
+    updateScore(){
+        this.score+=10;
+        CURRENT_SCORE.innerHTML=`Score: ${this.score}`;
+    }
+    /*
+    *                   makeBigger()
+    * Creates new cell, adds to array and appends to borad;
+    */
+    makeBigger(){
+        const nextCell=document.createElement(`div`);
+        nextCell.classList.add(`snake-cell`);
+        board.appendChild(nextCell);
+        this.snake.push(nextCell);
+    }
+    /*
+    * 
+    * Checks if the snake hits a wall or its cell. If so, @returns true;
+    * @returns false by default;
+    */
     checkIfDied(){
         const X=this.snakeHead.offsetLeft;
         const Y=this.snakeHead.offsetTop;
@@ -85,54 +176,6 @@ class Snake{
                 }
         }
         return false;
-    }
-    move(){
-        for(let i=this.snake.length-1; i>0; i--){
-            this.snake[i].style.left=`${this.snake[i-1].offsetLeft}px`;
-            this.snake[i].style.top=`${this.snake[i-1].offsetTop}px`;
-        }
-        switch(this.direction){
-            case LEFT:
-                this.snakeHead.style.left=`${this.snakeHead.offsetLeft-10}px`;
-                break;
-            case UP:
-                this.snakeHead.style.top=`${this.snakeHead.offsetTop-10}px`;
-                break;
-            case RIGHT:
-                this.snakeHead.style.left=`${this.snakeHead.offsetLeft+10}px`;
-                break;
-            case DOWN:
-                this.snakeHead.style.top=`${this.snakeHead.offsetTop+10}px`;
-                break;
-            default:
-                alert('DIRECTION ERROR');
-        }
-        this.lastDirection=this.direction;
-    }
-    updateScore(){
-        this.score+=10;
-        CURRENT_SCORE.innerHTML=`Score: ${this.score}`;
-    }
-    makeBigger(){
-        const nextCell=document.createElement(`div`);
-        nextCell.classList.add(`snake-cell`);
-        board.appendChild(nextCell);
-        this.snake.push(nextCell);
-    }
-    checkIfAteFood(){
-        if(this.snakeHead.offsetLeft===this.food.offsetLeft && 
-            this.snakeHead.offsetTop===this.food.offsetTop){
-                return true;
-            }
-        return false;
-    }
-    createFood(){
-        let X=Math.floor(Math.random()*BOARD_WIDTH);
-        let Y=Math.floor(Math.random()*BOARD_HEIGHT);
-        X-=(X%10);
-        Y-=(Y%10);
-        food.style.left=`${X}px`;
-        food.style.top=`${Y}px`;
     }
 }
 
